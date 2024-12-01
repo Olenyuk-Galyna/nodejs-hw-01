5bea2e4562738a4d9e0ee962c1096953c4eda42f
galyna <galaibidag@gmail.com>
2024-12-01 16:12:34 +0200

    add ts file

---
+++ src/utils/writeContacts.js
@@ -0,0 +1,7 @@
+import * as fs from 'node:fs/promises';
+
+import { PATH_DB } from '../constants/contacts.js';
+
+export const writeContacts = (contacts) => {
+  fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+};
